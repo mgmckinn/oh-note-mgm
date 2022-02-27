b660c9bf4c7c11428cfe5a21cab4a0addae3421a
Mitch McKinney <mgmckinn2325@me.com>
2022-02-27 00:42:14 -0500

    update js create note js

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+const noteRoutes = require('./noteRoutes');
+
+router.use(noteRoutes);
+module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,18 @@
-//List of dependencies
+//import variables
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
-//Express app initialization
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/apiRoutes/index');
+const PORT = process.env.PORT || 5500;
 const app = express();
-const PORT = process.env.PORT || 3000;
-//Data parsing options
-app.use(express.urlencoded({ extended: true }));
+
+
+app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-app.use(express.static(__dirname));
-//Require routes
-require('.routes/routes')(app);
-//Setup application Listener
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+
 app.listen(PORT, () => {
-    console.log('Application listening on PORT: ' + PORT);
-});
+  console.log(`API server now live on PORT ${PORT}! `)
+})
