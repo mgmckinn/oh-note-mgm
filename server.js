caaa414d4339aec43010f12be20ded1cb4e427ca
Mitch McKinney <mgmckinn2325@me.com>
2022-03-03 22:49:10 -0500

    remove duplicate files, fix js, ammend routes

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 //import variables
-const express = require('express');
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/apiRoutes/index');
-const PORT = process.env.PORT || 3002;
+const express = require('express')
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
 
 
-app.listen(PORT, () => {
-  console.log(`API server now live on PORT ${PORT}! `)
-})
+app.listen(PORT, function() {
+    console.log(`Listening to PORT: ${PORT}`);
+});
+
+
